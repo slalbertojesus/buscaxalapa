e1f62397865f554cebe4df0a4512ff12f9b213db
Alberto Sánchez <slalbertojesus@gmail.com>
2019-07-26 23:55:03 -0600

    Connection with postgress database added

--- src/py/app.py
+++ src/py/app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app  =  Flask(__name__)
 
-@app.route("/")
-def  Index():
-    return  render_template("Sessions.html")
+engine = create_engine("postgres://postgres:Barbara1621@localhost:5432/BuscaXalapa")
+db = scoped_session(sessionmaker(bind=engine))
 
-@app.route("/Service")
-def Service():
-    return render_template("Service.html")
+@app.route("/Index", methods = ["POST"])
+def index():
+    return render_template("Index.html")
 
-@app.route("/Homepage")
-def  Homepage():
-    return render_template("HomePage.html")
+@app.route("/Ingresar", methods = ["POST"])
+def  Ingresar():
+    return  render_template("Index.html")
