30439837e117cd2bbf1baf73962b4228f58b98ad
Alberto Sánchez <slalbertojesus@gmail.com>
2019-08-11 21:48:39 -0700

    Added routing for blueprints

--- buscaxalapa/__init__.py
+++ buscaxalapa/__init__.py
@@ -1,14 +1,12 @@
 import os
 
 from flask import Flask
-
 import buscaxalapa.views
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    from flaskr import views 
+    from buscaxalapa import views 
     app.register_blueprint(views.bp)
     app.add_url_rule("/", endpoint="index")
     app.run(host='0.0.0.0', port = 5000, threaded=True, debug=True)
-    
     return app
