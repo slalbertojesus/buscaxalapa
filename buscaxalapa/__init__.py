7c83123141031ec74aed9c5877a02f378653d891
Alberto Sánchez <slalbertojesus@gmail.com>
2019-08-16 21:54:16 -0700

    Added registry blueprint

--- buscaxalapa/__init__.py
+++ buscaxalapa/__init__.py
@@ -2,11 +2,14 @@ import os
 
 from flask import Flask
 import buscaxalapa.views
+import buscaxalapa.register
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     from buscaxalapa import views 
+    from buscaxalapa import register
     app.register_blueprint(views.bp)
+    app.register_blueprint(register.bp)
     app.add_url_rule("/", endpoint="index")
     app.run(host='0.0.0.0', port = 5000, threaded=True, debug=True)
     return app
--- buscaxalapa/views.py
+++ buscaxalapa/views.py
@@ -38,38 +38,3 @@ def Ingreso():
                 {"correo": correo, "nombre": nombre, "contra": contra_hash})
      db.commit()
      return render_template("Correct-Process.html", message="Registro exitoso")
-    
-# #Metodo en registro
-# @app.route("/Registro", methods = ["GET","POST"])
-# def Registro():
-#     if request.method == 'POST':
-#         rutaPrevia =    request.path
-#         if request.form['button-choosing'] == 'Anunciar mi negocio':
-#             return redirect(url_for("Negocio", rutaPrevia = rutaPrevia))   
-#         elif request.form['button-choosing'] == 'Anunciar mi servicio':
-#             return redirect(url_for("Servicio", rutaPrevia = rutaPrevia))
-#     if request.method == 'GET':
-#         return render_template("Registro.html")
-#     return redirect("Error")
-
-# @app.route("/Error")
-# def RoutError():
-#     return redirect("Correct-Process.html")
-
-# @app.route("/<rutaPrevia>/Negocio")
-# def Negocio(rutaPrevia):
-#     return render_template("/Business.html")
-    
-# @app.route("/<rutaPrevia>/Servicio")
-# def Servicio(rutaPrevia):
-#     return render_template("/Service.html")
-
-# #TODO Separar archivos 
-# @app.route("/Registro/Negocio", methods = ["POST"])
-# def redireccionaFormatoNegocio():
-#     identificadorNegocio = request.form['negocio-escogido']
-#     return redirect(url_for('despliegaFormaNegocio', identificadorNegocio = identificadorNegocio))
-
-# @app.route("/Registro/Negocio/<identificadorNegocio>")
-# def despliegaFormaNegocio(identificadorNegocio):
-#         return render_template("/Business-Creation.html")
