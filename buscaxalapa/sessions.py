27bb789e392059b611e5ce1c05f12e7c31c2b6aa
Alberto Sánchez <slalbertojesus@gmail.com>
2019-08-19 19:27:58 -0700

    Added sign up button with style

--- buscaxalapa/sessions.py
+++ buscaxalapa/sessions.py
@@ -8,11 +8,9 @@ bp = Blueprint("sessions", __name__)
 engine = create_engine("postgres://postgres:Barbara1621@localhost:5432/BuscaXalapa")
 db = scoped_session(sessionmaker(bind=engine))
 
-@bull.route("/login", methods=["GET", "POST"])
+
+@bp.route("/login", methods=["GET", "POST"])
 def login():
-    """For GET requests, display the login form. 
-    For POSTS, login the current user by processing the form.
-    print db
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.get(form.email.data)
@@ -23,4 +21,11 @@ def login():
                 db.session.commit()
                 login_user(user, remember=True)
                 return redirect(url_for("bull.reports"))
-    return render_template("login.html", form=form)
+return render_template("login.html", form=form)
+
+# TODO Añadir clases de usuario
+# TODO Crear dao para acceso a db 
+# TODO Separar metodos para abrir y cerrar conexionens en db 
+# TODO Checar password hasheada
+# TODO Revisar que es lo que va a cambiar cuando el usuario esté logueado
+# TODO Hacer cambios al header para que se cambie lo de iniciar sesion - ingresar
